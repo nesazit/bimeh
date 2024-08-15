1e7ef456945f42e56c4aa5da754131b975fa85e0
nesazit <nesazit@gmail.com>
2024-08-15 09:37:37 +0330

    vuejs configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,29 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import DefineOptions from "unplugin-vue-define-options/vite";
+const compositionResolver = (name) => {
+    const isCompositionApi = name.startsWith("use") && name != "useRoute";
+
+    if (isCompositionApi) {
+        return `@/Composables/${name}.ts`;
+    }
+};
+
+const layoutResolver = (name) => {
+    const isLayout = name.endsWith("Layout");
+    if (isLayout) {
+        return `@/Layouts/${name}.vue`;
+    }
+};
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.ts',
-            ssr: 'resources/js/ssr.ts',
+            input: "resources/js/app.ts",
+            ssr: "resources/js/ssr.ts",
             refresh: true,
         }),
         vue({
@@ -17,5 +34,21 @@ export default defineConfig({
                 },
             },
         }),
+        AutoImport({
+            imports: ["vue", "@vueuse/core"],
+            resolvers: [compositionResolver, layoutResolver],
+        }),
+
+        Components({
+            dirs: ["resources/js/Components"],
+            directoryAsNamespace: true,
+        }),
+
+        DefineOptions(),
     ],
+    resolve: {
+        alias: {
+            "@": "/resources/js",
+        },
+    },
 });
